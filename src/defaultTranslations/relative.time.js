7a90e20896551efae0fdfdf6c8dcc8ce0292c051
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-04-06 19:33:52 +0700

    Added Dutch , French and Thai translations

--- src/defaultTranslations/relative.time.js
+++ src/defaultTranslations/relative.time.js
@@ -40,5 +40,68 @@ export default {
       "day_in": "in __count__ Tag",
       "day_in_plural": "in __count__ Tagen"
     }
+  },
+  nl: {
+    translation: {
+      "now": "zonet",
+      "second_ago": "__count__ seconde geleden",
+      "second_ago_plural": "__count__ seconden geleden",
+      "second_in": "in __count__ seconde",
+      "second_in_plural": "in __count__ seconden",
+      "minute_ago": "__count__ minuut geleden",
+      "minute_ago_plural": "__count__ minuten geleden",
+      "minute_in": "in __count__ minuut",
+      "minute_in_plural": "in __count__ minuten",
+      "hour_ago": "__count__ uur geleden",
+      "hour_ago_plural": "__count__ uren geleden",
+      "hour_in": "in __count__ uur",
+      "hour_in_plural": "in __count__ uren",
+      "day_ago": "__count__ dag geleden",
+      "day_ago_plural": "__count__ dagen geleden",
+      "day_in": "in __count__ dag",
+      "day_in_plural": "in __count__ dagen"
+    }
+  },
+  fr: {
+    translation: {
+      "now": "juste",
+      "second_ago": "__count__ seconde passé",
+      "second_ago_plural": "__count__ secondes passé",
+      "second_in": "en __count__ seconde",
+      "second_in_plural": "en __count__ secondes",
+      "minute_ago": "__count__ minute passé",
+      "minute_ago_plural": "__count__ minutes passé",
+      "minute_in": "en __count__ minute",
+      "minute_in_plural": "en __count__ minutes",
+      "hour_ago": "__count__ heure passé",
+      "hour_ago_plural": "__count__ heures passé",
+      "hour_in": "en __count__ heure",
+      "hour_in_plural": "en __count__ heures",
+      "day_ago": "__count__ jour passé",
+      "day_ago_plural": "__count__ jours passé",
+      "day_in": "en __count__ jour",
+      "day_in_plural": "en __count__ jours"
+    }
+  },
+  th: {
+    translation: {
+      "now": "เมื่อกี้",
+      "second_ago": "__count__ วินาที ที่ผ่านมา",
+      "second_ago_plural": "__count__ วินาที ที่ผ่านมา",
+      "second_in": "อีก __count__ วินาที",
+      "second_in_plural": "อีก __count__ วินาที",
+      "minute_ago": "__count__ นาที ที่ผ่านมา",
+      "minute_ago_plural": "__count__ นาที ที่ผ่านมา",
+      "minute_in": "อีก __count__ นาที",
+      "minute_in_plural": "อีก __count__ นาที",
+      "hour_ago": "__count__ ชั่วโมง ที่ผ่านมา",
+      "hour_ago_plural": "__count__ ชั่วโมง ที่ผ่านมา",
+      "hour_in": "อีก __count__ ชั่วโมง",
+      "hour_in_plural": "อีก __count__ ชั่วโมง",
+      "day_ago": "__count__ วัน ที่ผ่านมา",
+      "day_ago_plural": "__count__ วัน ที่ผ่านมา",
+      "day_in": "อีก __count__ วัน",
+      "day_in_plural": "อีก __count__ วัน"
+    }
   }
-};;
+};
